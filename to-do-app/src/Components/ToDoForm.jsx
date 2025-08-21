e6196868d0a2721983f1e36a1acf41dea7dc2aeb
manii59 <maninder50999@gmail.com>
2025-08-21 11:11:44 +0530

    Updated input text of input component

--- to-do-app/src/Components/ToDoForm.jsx
+++ to-do-app/src/Components/ToDoForm.jsx
@@ -32,7 +32,7 @@ export const ToDoForm = ({ onAddTodo }) => {
           <input
             type="text"
             className="input-text"
-            placeholder="Enter the task you want to store"
+            placeholder="Enter the task you want"
             onChange={(event) => inputHandler(event.target.value)}
             value={input.content}
           />
